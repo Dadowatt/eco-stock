2dd73d1c7a43d10f4c2d0ce72e0b22265e7f881d
Dadowatt <dadowatt92@gmail.com>
2026-07-05 15:51:51 +0200

    feat: configuration de l'administration des modèles

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Warehouse, Product
+
+@admin.register(Warehouse)
+class WarehousAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "location", "capacity")
+    search_fields = ("name", "location")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "quantity", "status", "expiration_date", "warehouse",)
+    list_filter = ("status", "warehouse")
+    search_fields = ("name",)
