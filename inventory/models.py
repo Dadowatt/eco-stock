5877d80cc74a59e944a2da0013804edd73b60649
Dadowatt <dadowatt92@gmail.com>
2026-07-05 14:50:56 +0200

    Ajout des modèles Warehouse et Product avec leurs relations

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Warehouse(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=255)
+    capacity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Product(models.Model):
+    STATUS_CHOICES = [
+        ('available', 'Disponible'),
+        ('reserved', 'Réservé'),
+        ('expired', 'Périmé'),
+    ]
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    expiration_date = models.DateField()
+    status = models.CharField(max_length=20, choices = STATUS_CHOICES)
+    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE, related_name="products")
+
+    def __str__(self):
+        return self.name
