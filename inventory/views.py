1fac7e10c0f37bcd6d7f256bb3cf0626908ac345
Dadowatt <dadowatt92@gmail.com>
2026-07-05 20:21:53 +0200

    feat: implémentation du CRUD des entrepôts et des produits avec ModelViewSet

--- inventory/views.py
+++ inventory/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Warehouse, Product
+from .serializers import WarehouseSerializer, ProductSerializer
 
-# Create your views here.
+class WarehouseViewSet(ModelViewSet):
+    queryset = Warehouse.objects.all()
+    serializer_class = WarehouseSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
